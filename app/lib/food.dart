f1996046fe5a96f3f46ff85692269b49a591a437
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-04-30 11:01:30 +0800

    initial commit

---
+++ app/lib/food.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Food {
+  int id;
+  String name;
+  String shopName;
+  double price;
+  String imgUrl;
+  int quantity;
+
+  Food({
+    @required this.id,
+    @required this.name,
+    @required this.shopName,
+    @required this.price,
+    @required this.imgUrl,
+    @required this.quantity,
+    this.quantity = 1,
+  });
+
+  void increaseQuantity() {
+    this.quantity++;
+  }
+
+  void decreaseQuantity() {
+    this.quantity--;
+  }
+}
