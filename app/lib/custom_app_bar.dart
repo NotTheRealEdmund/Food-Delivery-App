9042cef2ad6cef25f73dd87d39bc58c89043a513
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-05-02 20:56:06 +0800

    Allow a parameter into CustomAppBar constructor to show count

--- app/lib/custom_app_bar.dart
+++ app/lib/custom_app_bar.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
+  String count;
+  CustomAppBar(this.count);
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(bottom: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Icon(Icons.menu),
-            GestureDetector(
-                onTap: () {},
-                child: Container(
-                  margin: EdgeInsets.only(right: 30),
-                  child: Text("0"),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      color: Colors.orange[300],
-                      borderRadius: BorderRadius.circular(50)),
-                ))
-          ],
-        ));
+    return Padding(
+      padding: EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          new Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.menu),
+                GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.only(right: 30),
+                      child: Text(count.toString()),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Colors.orange[300],
+                          borderRadius: BorderRadius.circular(50)),
+                    ))
+              ],
+            )
+          ),
+        ],
+      ),
+    );
   }
 }
