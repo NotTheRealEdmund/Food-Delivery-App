5868b056c94d4d920c6a15df4c1d612eb021d3d2
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2020-05-04 12:20:25 +0800

    Create a ListView of Cards with a row of item details

--- app/lib/checkout.dart
+++ app/lib/checkout.dart
@@ -16,8 +16,32 @@ class Checkout extends StatelessWidget {
             BackArrow(),
             SizedBox(height: 30),
             Text("Your orders", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35)),
-            SizedBox(height: 30),
-            
+            SizedBox(height: 45),
+
+            SingleChildScrollView(
+              child: ListView.builder(
+                shrinkWrap: true,
+                primary: false,
+                itemCount: cartList.getList().length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        // Add image on the very left
+                        SizedBox(width: 60),
+                        Text(cartList.getList()[index].quantity.toString() + " x " + cartList.getList()[index].name,
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(width: 60),
+                        Text("\$" + (cartList.getList()[index].quantity * cartList.getList()[index].price).toStringAsFixed(2)),
+                        SizedBox(width: 60),
+                        // Add a delete button on the very right
+                      ],
+                    ),
+                  );
+                }
+              ),
+            ),
+
           ],
         ),
       ),
